2cca9ccf9ba980fac70edb310ec967242f942f5b
juliarose <julialunarose@gmail.com>
2022-01-27 21:49:30 -0500

    added poll data for manager

--- src/trade_offer_manager.rs
+++ src/trade_offer_manager.rs
@@ -1,33 +1,40 @@
+use std::{
+    sync::Arc,
+    collections::HashMap
+};
 use crate::{
     SteamTradeOfferAPI,
     APIError,
     ServerTime,
     OfferFilter,
+    TradeOfferState,
     time,
     response,
     request,
     types::{
         AppId,
         ContextId,
-        Inventory
+        Inventory, TradeOfferId
     }
 };
 use steamid_ng::SteamID;
 
 #[derive(Debug)]
-pub struct TradeOfferManager {
+pub struct TradeOfferManager<'o> {
     api: SteamTradeOfferAPI,
     last_poll: Option<ServerTime>,
     last_poll_full_update: Option<ServerTime>,
+    poll_data: HashMap<TradeOfferId, Arc<response::TradeOffer<'o>>>,
 }
 
-impl TradeOfferManager {
+impl<'o> TradeOfferManager<'o> {
 
     pub fn new(key: String) -> Self {
         Self {
             api: SteamTradeOfferAPI::new(key),
             last_poll: None,
             last_poll_full_update: None,
+            poll_data: HashMap::new(),
         }
     }
 
@@ -59,7 +66,7 @@ impl TradeOfferManager {
         self.api.get_inventory(steamid, appid, contextid, tradable_only).await
     }
 
-    pub async fn do_poll<'a>(&'a mut self, full_update: bool) -> Result<Vec<response::TradeOffer<'a>>, APIError> {
+    pub async fn do_poll(&'o mut self, full_update: bool) -> Result<Poll<'o>, APIError> {
         fn last_poll_outdated(last_poll_update: Option<ServerTime>) -> bool {
             match last_poll_update {
                 Some(last_poll_full_update) => last_poll_full_update.timestamp()  >= 120000,
@@ -79,7 +86,49 @@ impl TradeOfferManager {
         }
         
         let historical_cutoff = time::timestamp_to_server_time(offers_since);
+        let offers = self.api.get_trade_offers(&filter, &Some(historical_cutoff)).await?;
 
-        self.api.get_trade_offers(&filter, &Some(historical_cutoff)).await
+        let mut poll = Poll {
+            new: Vec::new(),
+            changed: Vec::new(),
+        };
+
+        for offer in offers {
+            match self.poll_data.get(&offer.tradeofferid) {
+                Some(poll_offer) => {
+                    if poll_offer.trade_offer_state != offer.trade_offer_state {
+                        let offer = Arc::new(offer);
+
+                        poll.changed.push(PollChange {
+                            old_state: poll_offer.trade_offer_state.clone(),
+                            new_state: offer.trade_offer_state.clone(),
+                            offer: Arc::clone(&offer),
+                        });
+                        
+                        self.poll_data.insert(offer.tradeofferid, Arc::clone(&offer));
+                    }
+                },
+                None => {
+                    let offer = Arc::new(offer);
+
+                    poll.new.push(Arc::clone(&offer));
+                    
+                    self.poll_data.insert(offer.tradeofferid, Arc::clone(&offer));
+                },
+            }
+        }
+
+        Ok(poll)
     }
+}
+
+pub struct Poll<'o> {
+    pub new: Vec<Arc<response::TradeOffer<'o>>>,
+    pub changed: Vec<PollChange<'o>>,
+}
+
+pub struct PollChange<'o> {
+    pub old_state: TradeOfferState,
+    pub new_state: TradeOfferState,
+    pub offer: Arc<response::TradeOffer<'o>>,
 }
