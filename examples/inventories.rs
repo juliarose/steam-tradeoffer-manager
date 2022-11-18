b49b4adcebca744d5ee8aa733c7f7ed5dceb9a8b
juliarose <julialunarose@gmail.com>
2022-11-18 13:23:17 +0900

    added load inventories example

---
+++ examples/inventories.rs
@@ -0,0 +1,36 @@
+use steam_tradeoffer_manager::{
+    TradeOfferManager,
+    SteamID,
+    chrono::Duration,
+};
+use dotenv::dotenv;
+use std::env;
+
+fn get_steamid(key: &str) -> SteamID {
+    let sid_str = env::var(key)
+        .unwrap_or_else(|_| panic!("{} missing", key));
+    
+    SteamID::from(sid_str.parse::<u64>().unwrap())
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    dotenv().ok();
+    
+    let steamid = get_steamid("STEAMID");
+    let key = env::var("API_KEY").expect("API_KEY missing");
+    let manager = TradeOfferManager::builder(steamid, key)
+        .identity_secret(String::from("secret"))
+        .cancel_duration(Duration::minutes(30))
+        .build();
+    let inventory = manager.get_inventory(
+        &steamid,
+        440,
+        2,
+        true,
+    ).await?;
+    
+    println!("{}", inventory.len());
+
+    Ok(())
+}
