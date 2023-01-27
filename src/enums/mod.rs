f7a68f0f87f709b1f28cf6f84eec7dea592551ef
juliarose <julialunarose@gmail.com>
2023-01-27 14:12:00 -0500

    remove TradeStatus enum

--- src/enums/mod.rs
+++ src/enums/mod.rs
@@ -1,7 +1,5 @@
 mod confirmation_method;
 mod trade_offer_state;
-mod trade_status;
 
 pub use confirmation_method::ConfirmationMethod;
 pub use trade_offer_state::TradeOfferState;
-pub use trade_status::TradeStatus;
