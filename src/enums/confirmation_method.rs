a11e4effecaa8fbe312be95dc8b118e588e75721
juliarose <julialunarose@gmail.com>
2023-01-29 02:00:04 -0500

    cleanup

--- src/enums/confirmation_method.rs
+++ src/enums/confirmation_method.rs
@@ -3,7 +3,7 @@ use serde_repr::{Serialize_repr, Deserialize_repr};
 use strum_macros::{Display, EnumString};
 
 /// The method of confirmation.
-#[derive(Debug,Serialize_repr, Deserialize_repr, Display, EnumString,  PartialEq, TryFromPrimitive, IntoPrimitive, Clone, Copy)]
+#[derive(Debug, Serialize_repr, Deserialize_repr, Display, EnumString, PartialEq, TryFromPrimitive, IntoPrimitive, Clone, Copy)]
 #[repr(u8)]
 pub enum ConfirmationMethod {
     None = 0,
