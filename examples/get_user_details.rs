39860ab5cc4427cc5690907fdfc127ca55499da1
juliarose <julialunarose@gmail.com>
2023-02-14 02:12:24 -0500

    improved get_user_details example

--- examples/get_user_details.rs
+++ examples/get_user_details.rs
@@ -18,9 +18,28 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     
     manager.set_cookies(&cookies);
     
-    // Passing in none assumes we are friends with the user.
+    // Passing in GetUserDetailsMethod::None assumes we are friends with the user.
     let user_details = manager.get_user_details(&steamid, GetUserDetailsMethod::None).await?;
+    // Passing a tradeofferid will convert it into GetUserDetailsMethod::TradeOfferId(tradeofferid)
+    // let user_details = manager.get_user_details(&steamid, 5746598837).await?;
+    // Passing an access token will convert it into GetUserDetailsMethod::Token(token)
+    // let user_details = manager.get_user_details(&steamid, "itfRpc6r").await?;
     
-    println!("{user_details:?}");
+    println!("Trade will result in escrow? {}", user_details.has_escrow().nope());
     Ok(())
+}
+
+// Not really necessary, this just makes true/false values display in a more human way.
+trait Nope {
+    fn nope(&self) -> &'static str;
+}
+
+impl Nope for bool {
+    fn nope(&self) -> &'static str {
+        if *self {
+            "yep"
+        } else {
+            "nope"
+        }
+    }
 }
